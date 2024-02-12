9ecf4c0fc64cef48d7bdd7b063bbfe7a8f58e83f
f-haces <fhacesga@CougarNet.UH.EDU>
2024-02-12 09:21:06 -0600

    Update sync workstation

---
+++ affinetransformation.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sympy import *
+
+def evalFuncs(a, currdict):
+    """Evaluates an array of SymPy function with an input dictionary
+
+    Args:
+        param1 (np.array [n_eqs, n_obs] of type or nested list): SymPy functions to be evaluated.
+        param2 (dict): Dictionary to evaluate simpy functions.
+
+    Returns:
+        array: evaluated expressions for array
+    """
+    a = Array(a).subs(currdict)
+    return a
+    
+class affineTransformation:
+    """
+    This class forms an affine transformation. The initialization method 
+    expects 1D numpy arrays, which are transformed into a 3D space to form 
+    A matrix and calculate transformation values
+
+    Args:
+        x_c (1d np.array): X coordinates in comparator system
+        y_c (1d np.array): Y coordinates in comparator system
+        x_f (1d np.array): X coordinates for fedicial system
+        y_f (1d np.array): Y coordinates for fedicial system
+        verbose (bool): whether to print values on init. default = True
+
+    Attributes:
+        x_cap (np.array): estimates for coordinates in feducial system
+        a (np.array): linearized value a
+        b (np.array): linearized value b
+        c (np.array): linearized value c
+        d (np.array): linearized value d
+        x_translation (np.array): value for translation in the x-direction
+        y_translation (np.array): value for translation in the x-direction
+        scalex (np.array): X scale value 
+        scaley (np.array): Y scale value 
+        rotation (np.array): rotation value 
+        nonortho (np.array): nonorthogonality 
+    """    
+    def __init__(self, x_c, y_c, x_f, y_f, verbose=False):
+        
+        # CONVERT EVERYTHING TO NUMPY ARRAY AND INDEX AS DEPTH
+        x_c = np.array(x_c)[np.newaxis, :]
+        y_c = np.array(y_c)[np.newaxis, :]
+        x_f = np.array(x_f)[np.newaxis, :]
+        y_f = np.array(y_f)[np.newaxis, :]
+        
+        # CREATE L-MATRIX TO BE 2Px1
+        l = np.dstack((x_f, y_f)).flatten()
+        
+        # CREATE A MATRIX
+        a = np.vstack((x_c, y_c, np.ones(x_c.shape), np.zeros(x_c.shape), np.zeros(x_c.shape), np.zeros(x_c.shape))).T
+        b = np.vstack((np.zeros(x_c.shape), np.zeros(x_c.shape), np.zeros(x_c.shape), x_c, y_c, np.ones(x_c.shape))).T
+
+        outl = list()
+        for i in range(x_c.size):
+            outl.append(a[i, :])
+            outl.append(b[i, :])
+            
+        # CONVERT LIST OF NP.ARRAYS TO NP.ARRAY
+        A = np.array(outl)
+        
+        # CALCULATE X_CAP
+        x_cap_temp = np.matmul(np.linalg.inv(np.matmul(A.T, A)), A.T)
+        x_cap = np.matmul(x_cap_temp, l)
+        
+        # CALCULATE SAVE VALUES FOR TRANSFORMATION PARAMETERS
+        self.x_cap = x_cap
+        self.a = x_cap[0]
+        self.b = x_cap[1]
+        self.c = x_cap[3]
+        self.d = x_cap[4]
+        self.x_translation = x_cap[2]
+        self.y_translation = x_cap[5]
+        self.scalex = np.sqrt(self.a ** 2 + self.c ** 2)
+        self.scaley = np.sqrt(self.b ** 2 + self.d ** 2)
+        top = (self.a * self.b + self.c*self.d)
+        bot = (self.a * self.b - self.c*self.d)
+        self.nonortho = np.arctan(top / bot)
+        self.rotation = np.arctan(self.c / self.a)
+        
+        self.matrix = np.array([[self.a, self.b, self.x_translation], 
+                                [self.c, self.d, self.y_translation],
+                                [     0,      0,                  1]])
+        
+        self.currdict = {
+            "a" : self.a,
+            "b" : self.b,
+            "c" : self.c,
+            "d" : self.d,
+            "x_translation" : self.x_translation,
+            "y_translation" : self.y_translation,
+            "scalex" : self.scalex, 
+            "scaley" : self.scaley,
+            "nonortho" : self.nonortho,
+            "rotation" : self.rotation
+        }
+        
+        if verbose:
+            print("\nAffine Transformation Parameters -------------------------")
+            for k, v in self.currdict.items():
+                print("{:<8} {:<15.3e}".format(k, v))
+        
+    def transform(self, x_c, y_c):
+        # CREATE A MATRIX
+        temp_matrix = np.array([[self.a, self.b], [self.c, self.d]])
+        # MULTIPLY TIMES WEIGHTS AND ADD TRANSLATION 
+        temp_out = temp_matrix @ np.vstack((x_c, y_c))
+        temp_out = temp_out.T + np.array([self.x_translation, self.y_translation])
+        # RETURN VALUES
+        return temp_out[:, 0], temp_out[:, 1]
+    
+def plotResiduals(model, x_c, y_c, x_f, y_f, ax=False, plotscale = 0.1, title=""):
+    
+    if isinstance(ax, bool):
+        fig, ax = plt.subplots()
+    
+    x_f_cap, y_f_cap = model.transform(x_c, y_c)
+
+    res_x = (x_f_cap - x_f).astype(np.float64)
+    res_y = (y_f_cap - y_f).astype(np.float64)
+    
+    rmse_x = np.sqrt(np.sum(res_x**2)/res_x.shape[0])
+    rmse_y = np.sqrt(np.sum(res_y**2)/res_y.shape[0])
+
+    ax.quiver(x_f, y_f, res_x, res_y, scale=plotscale)
+    ax.set_xlabel("X (mm)")
+    ax.set_xlim((-150, 150))
+    ax.set_ylim((-150, 150))
+    ax.set_ylabel("Y (mm)")
+    ax.set_title(title)
+    ax.set_title(f"Residual Plot (Scale = {1 / plotscale})\nx RMSE: {rmse_x:.3e}\ny RMSE: {rmse_y:.3e}")
+
+    return res_x, res_y, ax
